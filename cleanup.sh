d4c6de21eccf65d0c089f423003c939445bcb7cc
Adam Radford (aradford) <aradford@cisco.com>
2019-08-29 00:26:38 +1000

    tweak

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source env3/bin/activate
 jupyter nbconvert ./.notebooks/01_Platform_API.ipynb --to notebook --ClearOutputPreprocessor.enabled=True --stdout > ./lab-notebooks/01_Platform_API.ipynb
+jupyter nbconvert ./.notebooks/02_AssuranceAPI.ipynb --to notebook --ClearOutputPreprocessor.enabled=True --stdout > ./lab-notebooks/02_AssuranceAPI.ipynb
